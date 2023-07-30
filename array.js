0539ba04c7a66fee4e339522484bd979f6a4591b
mashuk tamim <tamimmashuk328@gmail.com>
2023-07-30 06:42:48 +0600

    learned fundamentals of js array

---
+++ array.js
@@ -0,0 +1,9 @@
+var friendName = ['ataure', 'shanto', "sajjad", 'tasin'];
+var picnicFee = [100, 200, 300, 400];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+// console.log(friendName)
+// console.log(picnicFee)
+// console.log(vowels)
+
+console.log(vowels.length)
---
+++ variable.js
@@ -0,0 +1,33 @@
+//numeric type variable
+var sunglassPrice = 499;
+var watchPrice = 1499;
+
+var subTotal = sunglassPrice + watchPrice;
+var total = '499'+1499;
+
+//string type variable
+var firstName = 'Mashuk';
+var lastName = 'Tamim';
+var fullName = firstName + ' ' + lastName;
+
+//boolean type variable
+var isCoder = true;
+
+//two types of number: Integer, Float
+
+//Math operation: +, -, *, /
+//Shorthand: +=, -=, ++, --
+
+//convert number type: parseInt, parseFloat
+
+//To specify numbers after decimal points: toFixed
+
+//remainder: %
+console.log(isCoder)
+console.log(sunglassPrice)
+//console.log(subTotal)
+console.log(sunglassPrice + watchPrice)
+console.log(total)
+
+console.log(firstName + ' ' + lastName)
+console.log(fullName)
